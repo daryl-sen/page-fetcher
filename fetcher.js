b9c0449ad6c702eb2c3a3a02040a225ca0b66f64
daryl <tsdaryl@gmail.com>
2021-01-13 19:31:08 -0800

    core portion done

--- fetcher.js
+++ fetcher.js
@@ -1,22 +1,25 @@
+// commandline arguments
 const args = process.argv.slice(2);
 
+// imports
 const fs = require('fs');
-
 const request = require('request');
 
-const fetchPageContents = function(url, callback) {
-  // cannot return value: async function
-  request('http://bugdiary.com/', (error, response, body) => {
-    console.log('async done');
-    callback(body);
+// fetch the page, call writeToFile to save the contents
+const fetchPageContents = function(url, callback, fileName) {
+  request(url, (error, response, body) => {
+    callback(body, fileName);
   });
 };
 
-const saveResults = function(content) {
-  // console.log(content);
-  pageContent = { content };
-  // console.log(pageContent);
+const writeToFile = function(content, fileName) {
+  fs.writeFile(fileName, content, function(err) {
+    if (err) {
+      throw err;
+    }
+    const fileSize = fs.statSync(fileName).size;
+    console.log(`Downloaded and saved ${fileSize} bytes to ${fileName}`);
+  });
 };
 
-
-fetchPageContents(args[0], saveResults);
+fetchPageContents(args[0], writeToFile, args[1]);
